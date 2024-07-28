f2c2290e92ab344431ef553b271babaad18ecfa6
Ryan Khetlyr <ryan_khetlyr@waed.uscourts.gov>
2024-07-28 10:53:33 -0700

    rust10x: :

---
+++ crates/libs/lib-rpc/src/lib.rs
@@ -0,0 +1,13 @@
+// region:    --- Modules
+
+mod error;
+mod rpc_params;
+mod rpc_result;
+mod rpcs;
+
+pub use self::error::{Error, Result};
+pub use rpc_params::*;
+
+pub use rpcs::*;
+
+// endregion: --- Modules
